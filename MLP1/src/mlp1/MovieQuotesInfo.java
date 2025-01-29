c17d774fd7832994c3cc080de6ef9394a4cd57d5
GrandFatherMoon <71199842+GrandFatherMoon@users.noreply.github.com>
2025-01-28 23:26:24 -0500

    Add files via upload

---
+++ MLP1/src/mlp1/MovieQuotesInfo.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package mlp1;
+
+/**File name: MovieQuotesInfo.java
+ *Author: Emre Batir
+ * Purpose: Displays movies quote, the movie it came from, the character who
+ * said it, and the year the movie came out
+ */
+public class MovieQuotesInfo {
+    
+    public static void main(String[] args){
+        
+        System.out.println("What's the most you ever lost on a coin toss?");
+        System.out.println("No Country for Old Men");
+        System.out.println("Anton Chigurh");
+        System.out.println("2007");
+    
+    }
+    
+}
---
+++ MLP1/src/mlp1/SongLyrics.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package mlp1;
+
+/**File name:SongLyrics.java
+ *Author: Emre Kaan Batir
+ * Purpose: Displays lyrics to song
+ */
+public class SongLyrics {
+
+    
+    public static void main(String[] args) {
+        System.out.println("Who let the dogs out?");
+        System.out.println("Who, who, who, who, who?");
+        System.out.println("Who let the dogs out?");
+        System.out.println("Who, who, who, who, who?");
+    }
+    
+}
